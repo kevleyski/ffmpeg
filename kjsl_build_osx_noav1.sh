25f43342ff7a3351d732668da64ddb022350f683
Kevin Staunton-Lambert <kevleyski@gmail.com>
2018-10-24 10:14:39 +1100

    Add OpenSSL

--- kjsl_build_osx_noav1.sh
+++ kjsl_build_osx_noav1.sh
@@ -22,6 +22,7 @@
  -L/usr/local/Cellar/theora/1.1.1/lib \
  -L/usr/local/Cellar/libvorbis/1.3.5/lib \
  -L/usr/local/Cellar/xvid/1.3.4/lib \
+ -I/usr/local/Cellar/openssl/1.0.2p/lib \
  -L/usr/local/lib/openjpeg-2.3/lib \
  -L/usr/lib" \
  --extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
@@ -30,6 +31,7 @@
  -I/usr/local/Cellar/libvorbis/1.3.5/include \
  -I/usr/local/Cellar/xvid/1.3.4/include \
  -I/usr/local/Cellar/sdl/1.2.15/include \
+ -I/usr/local/Cellar/openssl/1.0.2p/include \
  -I/usr/local/include/openjpeg-2.3/include \
  -I/usr/include"
 
