91bd10b0a685da91d59733a35c15cafc104b85e2
klambert <kevleyski@gmail.com>
2019-09-07 09:08:30 +1000

    [KJSL] HLS discos issue Adding force_dts_monotonicity from https://ffmpeg.org/pipermail/ffmpeg-devel/2018-March/226706.html http://ffmpeg.org/pipermail/ffmpeg-devel/attachments/20180316/739542e4/attachment.obj

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -4435,15 +4435,14 @@ static int process_input(int file_index)
         pkt.dts *= ist->ts_scale;
 
     pkt_dts = av_rescale_q_rnd(pkt.dts, ist->st->time_base, AV_TIME_BASE_Q, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-	printf("%d, force_dts_monotonicity", __LINE__, force_dts_monotonicity);
     if ((ist->dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO || ist->dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) &&
-            pkt_dts != AV_NOPTS_VALUE &&
-            ist->next_dts == AV_NOPTS_VALUE &&
-            !copy_ts &&
-            (is->iformat->flags & AVFMT_TS_DISCONT) &&
-            ifile->last_ts != AV_NOPTS_VALUE &&
-            !force_dts_monotonicity
-	) {
+        pkt_dts != AV_NOPTS_VALUE &&
+        ist->next_dts == AV_NOPTS_VALUE &&
+        !copy_ts &&
+        (is->iformat->flags & AVFMT_TS_DISCONT) &&
+        ifile->last_ts != AV_NOPTS_VALUE &&
+        !force_dts_monotonicity
+    ) {
         int64_t delta   = pkt_dts - ifile->last_ts;
         if (delta < -1LL*dts_delta_threshold*AV_TIME_BASE ||
             delta >  1LL*dts_delta_threshold*AV_TIME_BASE){
@@ -4472,8 +4471,8 @@ static int process_input(int file_index)
          ist->dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) &&
          pkt_dts != AV_NOPTS_VALUE && ist->next_dts != AV_NOPTS_VALUE &&
         !copy_ts &&
-		!force_dts_monotonicity
-		) {
+        !force_dts_monotonicity
+    ) {
         int64_t delta   = pkt_dts - ist->next_dts;
         if (is->iformat->flags & AVFMT_TS_DISCONT) {
             if (delta < -1LL*dts_delta_threshold*AV_TIME_BASE ||
@@ -4510,6 +4509,8 @@ static int process_input(int file_index)
 
     if (pkt.dts != AV_NOPTS_VALUE)
         ifile->last_ts = av_rescale_q(pkt.dts, ist->st->time_base, AV_TIME_BASE_Q);
+
+
     if (force_dts_monotonicity &&
         (pkt.pts != AV_NOPTS_VALUE || pkt.dts != AV_NOPTS_VALUE) &&
         (ist->dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO || ist->dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO)
@@ -4532,23 +4533,20 @@ static int process_input(int file_index)
             }
         }
 
-        av_log(is, AV_LOG_DEBUG, "dts_error: %d, pts_error: %d, threshold: %d\n", ff_dts_error, ff_pts_error, ff_dts_threshold);
-
-        if(ff_dts_error > ff_dts_threshold || ff_dts_error < (-ff_dts_threshold) || ff_pts_error < (-ff_dts_threshold)) {
+        if(ff_dts_error > 0 || ff_dts_error < (-ff_dts_threshold) || ff_pts_error < (-ff_dts_threshold)) {
             if(pkt.dts == AV_NOPTS_VALUE /*  || ist->next_dts != AV_NOPTS_VALUE */ ) {
                 pkt.pts += ff_pts_error;
                 ifile->ff_timestamp_monotonicity_offset += ff_pts_error;
-                av_log(is, AV_LOG_WARNING, "Incoming PTS error %"PRId64", offsetting subsequent timestamps by %"PRId64" to correct\n", ff_pts_error, ifile->ff_timestamp_monotonicity_offset);
+                av_log(is, AV_LOG_INFO, "Incoming PTS error %"PRId64", offsetting subsequent timestamps by %"PRId64" to correct\n", ff_pts_error, ifile->ff_timestamp_monotonicity_offset);
             }
             else {
                 pkt.pts += ff_dts_error;
                 pkt.dts += ff_dts_error;
                 ifile->ff_timestamp_monotonicity_offset += ff_dts_error;
-                av_log(is, AV_LOG_WARNING, "Incoming DTS error %"PRId64", offsetting subsequent timestamps by %"PRId64" to correct\n", ff_dts_error, ifile->ff_timestamp_monotonicity_offset);
+                av_log(is, AV_LOG_INFO, "Incoming DTS error %"PRId64", offsetting subsequent timestamps by %"PRId64" to correct\n", ff_dts_error, ifile->ff_timestamp_monotonicity_offset);
             }
         }
     }
-
 
     if (debug_ts) {
         av_log(NULL, AV_LOG_INFO, "demuxer+ffmpeg -> ist_index:%d type:%s pkt_pts:%s pkt_pts_time:%s pkt_dts:%s pkt_dts_time:%s off:%s off_time:%s\n",
