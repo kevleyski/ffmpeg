191cad2b06f84f0a92991ed9246be95bae73f360
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2021-02-20 12:35:27 +1100

    KJSL: FFmpeg debug

--- kjsl_build_osx_noav1.sh
+++ kjsl_build_osx_noav1.sh
@@ -2,13 +2,6 @@
 
 # KJSL: script to build FFmpeg on OSX
 
-#if [ ! -d /usr/local/include/openjpeg-2.3/include ]; then
-#cd openjpeg 
-#./configure
-#make
-#sudo make install
-#fi
-
 # build FFmpeg 4 (with AV1)
  PKG_CONFIG_PATH="/usr/local/lib/pkgconfig" ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
  --enable-libass --enable-libfdk-aac --enable-libfreetype \
@@ -17,20 +10,22 @@
  --enable-libvpx --enable-libx264 --enable-libx265 --enable-libxvid --enable-ffplay --enable-sdl2 \
  --enable-libmp3lame \
  --enable-libsrt --enable-librtmp \
- --extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
- -L/usr/local/Cellar/libogg/1.3.2/lib \
- -L/usr/local/Cellar/theora/1.1.1/lib \
- -L/usr/local/Cellar/libvorbis/1.3.5/lib \
- -L/usr/local/Cellar/xvid/1.3.4/lib \
- -I/usr/local/Cellar/openssl/1.0.2p/lib \
+ --extra-ldflags="-L/usr/local/opt/lame/lib \
+ -L/usr/local/opt/libogg/lib \
+ -L/usr/local/opt/theora/lib \
+ -L../srt \
+ -L/usr/local/opt/libvorbis/lib \
+ -L/usr/local/opt/xvid/lib \
+ -I/usr/local/opt/openssl/lib \
  -L/usr/lib" \
- --extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
- -I/usr/local/Cellar/libogg/1.3.2/include \
- -I/usr/local/Cellar/theora/1.1.1/include \
- -I/usr/local/Cellar/libvorbis/1.3.5/include \
- -I/usr/local/Cellar/xvid/1.3.4/include \
- -I/usr/local/Cellar/sdl/1.2.15/include \
- -I/usr/local/Cellar/openssl/1.0.2p/include \
+ --extra-cflags="-I/usr/local/opt/lame/include \
+ -I/usr/local/opt/libogg/include \
+ -I/usr/local/opt/theora/include \
+ -I../srt/srtcore \
+ -I/usr/local/opt/libvorbis/include \
+ -I/usr/local/opt/xvid/include \
+ -I/usr/local/opt/sdl/include \
+ -I/usr/local/opt/openssl/include \
  -I/usr/include"
 
 make -j5
