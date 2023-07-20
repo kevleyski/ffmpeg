572979712b3b0d2cbbe60d3ddebc1c6a810dab7a
Kevin Staunton-Lambert <kevstal@meta.com>
2023-07-20 13:35:20 +1000

    KJSL: FFmpeg debug

--- kjsl_build_osx.sh
+++ kjsl_build_osx.sh
@@ -2,30 +2,29 @@
 
 # KJSL: script to build FFmpeg on OSX
 
-brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 xvid yasm
+if [ ! -d /usr/local/Cellar/theora ]; then
+  brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 x265 xvid nasm yasm
+fi
 
-curl -O -L https://github.com/kevleyski/aom/archive/master.zip
-unzip master.zip
-cd aom-master
-./configure --prefix="$HOME/ffmpeg_build" --cpu=sse3 --enable-av1 --disable-shared
-make
-make install
-cd ..
+LIBFFI_CFLAGS=-I/usr/include/ffi LIBFFI_LIBS=-lffi ./configure;make && sudo make install
+GLIB_CFLAGS="-I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include" GLIB_LIBS="-lglib-2.0 -lgio-2.0" ./configure --with-pc-path="/usr/X11/lib/pkgconfig:/usr/X11/share/pkgconfig:/usr/lib/pkgconfig:/usr/local/lib/pkgconfig"
 
-# build FFmpeg 4 (with AV1)
+# build FFmpeg (with AV1, srt, tesseract and)
+ export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
  ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
- --enable-libass --enable-libfdk-aac --enable-libfreetype \
+ --enable-libfdk-aac --enable-libfreetype \
  --enable-libopus --enable-libtheora --enable-libvorbis \
+ --enable-libopenjpeg --enable-avfilter \
  --enable-libvpx --enable-libx264 --enable-libx265 --enable-libxvid --enable-ffplay \
- --enable-libaom \
  --enable-libtesseract \
- --enable-libsrt --enable-librtmp \
+ --enable-libsrt \
+ --enable-librtmp \
+ --enable-libflite \
  --extra-ldflags="-L/usr/local/Cellar/lame/3.99.5/lib \
  -L/usr/local/Cellar/libogg/1.3.2/lib \
  -L/usr/local/Cellar/theora/1.1.1/lib \
  -L/usr/local/Cellar/libvorbis/1.3.5/lib \
  -L/usr/local/Cellar/xvid/1.3.4/lib \
- -L/usr/local/Cellar/aom/HEAD-285d941/lib \
  -L/usr/lib" \
  --extra-cflags="-I/usr/local/Cellar/lame/3.99.5/include \
  -I/usr/local/Cellar/libogg/1.3.2/include \
@@ -33,9 +32,8 @@ cd ..
  -I/usr/local/Cellar/libvorbis/1.3.5/include \
  -I/usr/local/Cellar/xvid/1.3.4/include \
  -I/usr/local/Cellar/sdl/1.2.15/include \
- -I/usr/local/Cellar/aom/HEAD-285d941/include \
  -I/usr/include"
 
 
-make -j5
+make -j$(($(nproc)+1))
 sudo make install
