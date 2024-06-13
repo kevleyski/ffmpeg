8cf41b04f652a3892fc4ec46b24731a9cfeb4641
Kevin Staunton-Lambert <kevstal@meta.com>
2024-06-13 10:30:59 +1000

    KJSL: Enhanced RTMP

--- kjsl_build_macos.sh
+++ kjsl_build_macos.sh
@@ -2,13 +2,14 @@
 
 # KJSL: script to build FFmpeg debug on MacOS
 
-if [ ! -d /usr/local/Cellar/theora ]; then
-  brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 x265 xvid nasm yasm openssl rtmpdump freetype graphite2 harfbuzz fontconfig fribidi
+if [ ! -d /opt/homebrew/Cellar/theora ]; then
+  brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html wget x264 x265 xvid nasm yasm openssl rtmpdump freetype graphite2 harfbuzz fontconfig fribidi coreutils
 fi
 
+export PKG_CONFIG_PATH=/opt/homebrew/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH
+
 # build FFmpeg (with AV1, srt, tesseract and)
-PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH LDFLAGS="-Wl,-ld_classic,-framework,CoreFoundation -Wl,-framework,Security -Wl,-framework,VideoToolbox -Wl,-framework,CoreMedia -Wl,-framework,CoreVideo" LIBFFI_CFLAGS=-I/usr/include/ffi LIBFFI_LIBS=-lffi ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
---pkgconfigdir=/usr/local/lib/pkg-config \
+LDFLAGS="-Wl,-ld_classic,-framework,CoreFoundation -Wl,-framework,Security -Wl,-framework,VideoToolbox -Wl,-framework,CoreMedia -Wl,-framework,CoreVideo" LIBFFI_CFLAGS=-I/usr/include/ffi LIBFFI_LIBS=-lffi ./configure  --prefix=/usr/local --enable-gpl --enable-nonfree \
 --enable-postproc \
 --enable-shared \
 --enable-pthreads \
@@ -21,15 +22,12 @@ PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH LDF
 --enable-libfribidi \
 --enable-libharfbuzz \
 --enable-libsvtav1 \
---enable-libopus --enable-libtheora --enable-libvorbis \
+--enable-libopus --enable-libvorbis \
 --enable-libopenjpeg --enable-avfilter \
---enable-libvpx --enable-libx264 --enable-libx265 --enable-libxvid --enable-ffplay \
+--enable-libvpx --enable-libx264 --enable-libx265 --enable-ffplay \
 --enable-libtesseract \
 --enable-libsrt \
---enable-gcrypt \
---enable-librtmp \
---enable-libflite \
---enable-libmp3lame
+--enable-librtmp
 
 make -j$(($(nproc)+1))
 sudo make install
